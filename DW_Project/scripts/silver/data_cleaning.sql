f6b0a469e53c672e85c813587072b833a351abea
Martin <luigi-metalcore@hotmail.com>
2025-11-05 14:52:52 -0600

    Updated Query

--- DW_Project/scripts/silver/data_cleaning.sql
+++ DW_Project/scripts/silver/data_cleaning.sql
@@ -29,6 +29,16 @@ SELECT * from row_table
 WHERE flag_last != 1
 
 
+-- Query to retrieve data without duplicates. 
+
+WITH cleaned_table AS (
+SELECT *, ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) AS flag_last
+FROM bronze.crm_cust_info )
+
+SELECT * from cleaned_table
+WHERE flag_last = 1
+
+
 
 -- Checking Customer Information Tables
 
@@ -53,3 +63,88 @@ WHERE cst_gndr != TRIM(cst_gndr)
 SELECT cst_marital_status
 FROM bronze.crm_cust_info
 WHERE cst_marital_status != TRIM(cst_marital_status)
+
+
+
+
+
+
+----------------------- Query to retrieve data without duplicates and without spaces. -----------------------
+
+WITH cleaned_table AS (
+SELECT cst_id, 
+	   cst_key, 
+	   TRIM(cst_firstname) AS cst_firstname , 
+	   TRIM(cst_lastname) AS cst_lastname, 
+	   cst_marital_status, 
+	   cst_gndr, 
+	   cst_create_date, 
+	   ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) AS flag_last
+FROM bronze.crm_cust_info )
+
+SELECT * from cleaned_table
+WHERE flag_last = 1
+
+--------------------------------------------------------------------------------------------------------------
+
+
+
+-- Data Cardinality, change the values that look ambiguous and confusing for something more formal and clear, Ex. M -> Male / M -> Married
+
+select *, 
+	   case
+	     when cst_marital_status = 'S' then 'Single'
+		 when cst_marital_status = 'M' then 'Married'
+		 else 'N/A'
+		end
+		
+from bronze.crm_cust_info
+
+where cst_marital_status is null 
+
+
+select *, 
+	   case
+	     when cst_gndr = 'F' then 'Female'
+		 when cst_gndr = 'M' then 'Male'
+		 else 'N/A'
+		end 
+from bronze.crm_cust_info
+
+
+--------- Query to retrieve data without duplicates and without spaces and with data cardinality name conventions applied, ---------------------
+--------- also noticed that the UPPER function was applied so that means we are making our values all upper case and will be unsensitive. ------
+
+WITH cleaned_table AS (
+SELECT cst_id, 
+	   cst_key, 
+	   TRIM(cst_firstname) AS cst_firstname , 
+	   TRIM(cst_lastname) AS cst_lastname, 
+	   case
+	     when UPPER(cst_marital_status) = 'S' then 'Single'
+		 when UPPER(cst_marital_status) = 'M' then 'Married'
+		 else 'N/A'
+		end as cst_marital_status,
+	    
+	   case
+	     when UPPER(cst_gndr) = 'F' then 'Female'
+		 when UPPER(cst_gndr) = 'M' then 'Male'
+		 else 'N/A'
+		end as cst_gndr,
+	   cst_create_date, 
+	   ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) AS flag_last
+FROM bronze.crm_cust_info )
+
+SELECT * from cleaned_table
+WHERE flag_last = 1
+order by 5,6
+
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+ 
+
